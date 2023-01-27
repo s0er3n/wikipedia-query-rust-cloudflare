8bf34ea9e704daca470d91183e0daa97b9c09ec9
Sören Michaels <soeren@Air-von-Soren.fritz.box>
2023-01-27 19:59:58 +0100

    faster parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use select::document::Document;
-use select::predicate::{Attr, Name};
+use reqwest::header::USER_AGENT;
 use serde::Serialize;
 use std::collections::{HashMap, HashSet};
+use tl::{queryselector::iterable::QueryIterable, HTMLTag, Node::Tag, NodeHandle, ParserOptions};
 use worker::*;
 
 mod utils;
@@ -14,6 +14,7 @@ fn log_request(req: &Request) {
         req.cf().coordinates().unwrap_or_default(),
         req.cf().region().unwrap_or_else(|| "unknown region".into())
     );
+    console_log!("header = {:?}", &req.headers());
 }
 
 #[derive(Clone, Serialize)]
@@ -26,22 +27,61 @@ struct Parse {
     text: HashMap<String, String>,
     title: String,
 }
-async fn make_query(target: &str) -> Wiki {
-    let resp_txt = reqwest::get(format!("https://en.wikipedia.org/wiki/{}?useskin=vector", target))
+async fn make_query(target: &str, headers_req: &Headers) -> Wiki {
+    let client = reqwest::Client::new();
+    let resp_txt = client
+        .get(format!(
+            "https://en.wikipedia.org/wiki/{}?useskin=vector",
+            target
+        ))
+        .header(USER_AGENT, &headers_req.get("user-agent").unwrap().unwrap())
+        .send()
         .await
         .unwrap()
         .text()
         .await
         .unwrap();
 
-    let wiki_page = Document::from(resp_txt.as_str());
+    let dom = tl::parse(&resp_txt, ParserOptions::default()).unwrap();
 
-    let title = wiki_page.find(Name("h1")).next().unwrap().text();
+    let parser = dom.parser();
+    let title = dom.query_selector("h1").unwrap().next().unwrap();
 
-    let article = wiki_page
-        .find(Attr("id", "mw-content-text"))
-        .next()
-        .unwrap();
+    let title = NodeHandle::get(&title, parser).unwrap().inner_text(parser);
+
+    // let title = wiki_page.find(Name("h1")).next().unwrap().text();
+    let article = dom
+        .get_element_by_id("mw-content-text")
+        .unwrap()
+        .get(parser)
+        .unwrap()
+        .inner_html(parser);
+
+    // let see_also_tag = dom
+    //     .query_selector("span#See_also")
+    //     .unwrap()
+    //     .next()
+    //     .unwrap()
+    //     .get(parser);
+
+    let content = article;
+    // match see_also_tag {
+    //     Some(Tag(span)) => {
+    //         let (mut start, _) = span.boundaries(parser);
+    //         loop {
+    //             start = start - 1;
+    //             if resp_txt.chars().nth(start).unwrap() == '<' {
+    //                 break;
+    //             }
+    //         }
+    //         &resp_txt[..start]
+    //     }
+    //     _ => &resp_txt,
+    // };
+
+    // .find(Attr("id", "mw-content-text"))
+    // .next()
+    // .unwrap();
 
     // let links: HashSet<String> = article
     //     .find(Name("a"))
@@ -57,15 +97,14 @@ async fn make_query(target: &str) -> Wiki {
     //     })
     //     .map(|x| String::from(&x[6..]))
     //     .collect();
-    let content = article.html();
     let mut hashmap = HashMap::new();
 
-    hashmap.insert("*".to_string(), content);
+    hashmap.insert("*".to_string(), content.to_string());
 
     return Wiki {
         parse: Parse {
             text: hashmap,
-            title,
+            title: title.into(),
         },
     };
 }
@@ -91,7 +130,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                 let cors = worker::Cors::new()
                     .with_origins(["*"])
                     .with_methods([Method::Get]);
-                let response_json = make_query(name).await;
+                let response_json = make_query(name, req.headers()).await;
                 return Response::from_json(&response_json)?.with_cors(&cors);
             }
 
